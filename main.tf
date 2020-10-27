63847aa9eace428aaad2344fa4022f456ce24107
Waleed <masterwali@Waleeds-MacBook-Pro.local>
2020-10-27 06:57:55 -0400

    source updated

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 module "web_server" {
-  source = "git@gitlab.com:cloudly-engineer/aws/tf-modules/iam-ec2-role.git"
+  source = "git@github.com:masterwali/tf-module-iam-ec2-role.git"
 
   ec2_iam_role_name  = "svc-web-server-role"
   policy_description = "IAM ec2 instance profile for the web servers."
